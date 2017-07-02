1136862b638b1f99af31b64c00554760ece14b1a
srajapaksha <srajapaksha@panviva.com>
2017-07-02 21:02:23 +1000

    Routing

---
+++ Seminda.CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Seminda.CityInfo.API.Controllers
+{
+    public class CitiesController:Controller 
+    {
+        [HttpGet("api/cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<Object>
+            {
+                new {Id=1, Name="Melbourne"},
+                new {Id=2, Name="Sydney"}
+            });
+        }
+    }
+}
