83a345e53c9d69b8996e968d7cf6b1f9cd31fcbc
srajapaksha <srajapaksha@panviva.com>
2017-07-08 15:01:59 +1000

    Add the point of interest API

---
+++ Seminda.CityInfo.API/Controllers/PointOfInterestController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Seminda.CityInfo.API.Data;
+using System.Linq;
+
+namespace Seminda.CityInfo.API.Controllers
+{
+    public class PointOfInterestController : Controller 
+    {
+        [HttpGet("api/cities/{cityId}/pointsofinterest")]
+        public IActionResult GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            return Ok(city.PointOfInterest);
+        }
+        
+        [HttpGet("api/cities/{cityId}/pointofinterest/{id}")]
+        public IActionResult GetPointOfInterest(int cityId,int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointOfInterest.FirstOrDefault(p => p.Id == id);
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+            return Ok(pointOfInterest);
+        }
+    }
+}
--- Seminda.CityInfo.API/Data/CitiesDataStore.cs
+++ Seminda.CityInfo.API/Data/CitiesDataStore.cs
@@ -16,19 +16,64 @@ namespace Seminda.CityInfo.API.Data
                 {
                     Id=1,
                     Name="Melbourne",
-                    Description="Melbourne is the coastal capital of the southeastern Australian state of Victoria"
+                    Description="Melbourne is the coastal capital of the southeastern Australian state of Victoria",
+                    PointOfInterest=new List<PointOfInterestDto>
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id=1,
+                            Name="Great Ocean Road",
+                            Description="The Great Ocean Road is an Australian National Heritage listed 243 kilometres stretch of road along the south-eastern coast of Australia between the Victorian cities of Torquay and Allansford."
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id=2,
+                            Name="Shrine of Remembrance",
+                            Description="The Shrine of Remembrance, located in Kings Domain on St Kilda Road, Melbourne, Australia was built as a memorial to the men and women of Victoria who served in World War I and is now a memorial to all Australians who have served in war."
+                        }
+                    }
                 },
                 new CityDto
                 {
                     Id=2,
                     Name="Sydney",
-                    Description="Sydney, capital of New South Wales"
+                    Description="Sydney, capital of New South Wales",
+                    PointOfInterest=new List<PointOfInterestDto>
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id=1,
+                            Name="Sydney Opera House",
+                            Description="The Sydney Opera House is a multi-venue performing arts centre in Sydney, Australia. It is one of the 20th century's most famous and distinctive buildings."
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id=2,
+                            Name="Darling Harbour",
+                            Description="Darling Harbour is a harbour adjacent to the city centre of Sydney, New South Wales, Australia."
+                        }
+                    }
                 },
                 new CityDto
                 {
                     Id=3,
                     Name="Brisbane",
-                    Description="Brisbane, capital of Queensland"
+                    Description="Brisbane, capital of Queensland",
+                    PointOfInterest=new List<PointOfInterestDto>
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id=1,
+                            Name="Story Bridge",
+                            Description="The Story Bridge is a heritage-listed steel cantilever bridge spanning the Brisbane River that carries vehicular, bicycle and pedestrian traffic between the northern and the southern suburbs of Brisbane, Queensland, Australia."
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id=2,
+                            Name="Lone Pine Koala Sanctuary",
+                            Description="Lone Pine Koala Sanctuary is an 18-hectare Koala Sanctuary in the Brisbane suburb of Fig Tree Pocket in Queensland, Australia. Founded in 1927, it is the world's oldest and largest koala sanctuary."
+                        }
+                    }
                 }
             };
         }
