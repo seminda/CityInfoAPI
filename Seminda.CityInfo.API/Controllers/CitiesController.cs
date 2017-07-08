eaa4ad5f56ac787060069768fd48238945375a86
Seminda Rajapaksha <seminda@gmail.com>
2017-07-08 15:03:10 +1000

    Feature/city info part2 (#1)
    * Adding the ASP.NET Core MVC Middleware
    
    * Routing
    
    * Improving the Architecture with Model Classes
    
    * Improving the Architecture with Model Classes
    
    * Returning Correct Status Codes
    
    * Add the point of interest API

---
+++ Seminda.CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Seminda.CityInfo.API.Data;
+using System.Linq;
+
+namespace Seminda.CityInfo.API.Controllers
+{
+    public class CitiesController:Controller 
+    {
+        [HttpGet("api/cities")]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+        
+        [HttpGet("api/cities/{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            return Ok(city);
+        }
+    }
+}
---
+++ Seminda.CityInfo.API/Models/CityDto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Seminda.CityInfo.API.Models
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int NumberOfPointOfInterest
+        {
+            get
+            {
+                return PointOfInterest.Count;
+            }
+        }
+        public ICollection<PointOfInterestDto> PointOfInterest { get; set; } 
+            = new List<PointOfInterestDto>();
+    }
+}
